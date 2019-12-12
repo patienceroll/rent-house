dbfe87f004b46e5f4a9c05820789610e676651af
张显磊 <1079105171@qq.com>
2019-12-12 15:26:22 +0800

    初始化项目

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Home from './pages/Home';
+import Citys from './pages/Citys';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+
+const App = (props) => {
+    return (
+        <Router>
+            <ul>
+                <li>
+                    <Link to='/home'>首页</Link>
+                </li>
+                <li>
+                    <Link to='/cityList'>City</Link>
+                </li>
+            </ul>
+            <Route path='/home'><Home></Home></Route>
+            <Route path='/cityList'><Citys></Citys></Route>
+        </Router>
+    )
+}
+export default App;
---
+++ src/pages/Citys.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Citys() {
+  return (
+    <h1>
+      城市页面
+    </h1>
+  )
+}
+
+export default Citys;
